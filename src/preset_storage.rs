f002bdd6aba9474d46f02525cf31ccb5db6b9355
Datttta <diogenesresplandes@gmail.com>
2026-07-30 15:08:03 -0300

    Preset feature finished

--- src/preset_storage.rs
+++ src/preset_storage.rs
@@ -1,20 +1,32 @@
 use std::fs;
-use std::path::Path;
+use directories::ProjectDirs;
 
 use crate::presets::Preset;
 
 const FILE_NAME: &str = "presets.json";
 
-pub fn save_preset(presets: &[Preset]) -> Result<(), Box<dyn std::error::Error>> {
+fn presets_path() -> std::path::PathBuf {
+    let proj_dirs = ProjectDirs::from("", "", "Duetime")
+        .expect("Could not determine config directory");
+
+    let config_dir = proj_dirs.config_dir();
+
+    fs::create_dir_all(config_dir)
+        .expect("Could not create config directory");
+
+    config_dir.join(FILE_NAME)
+}
+
+pub fn save_preset(
+    presets: &[Preset],
+) -> Result<(), Box<dyn std::error::Error>> {
     let json = serde_json::to_string_pretty(presets)?;
-
-    fs::write(FILE_NAME, json)?;
-
+    fs::write(presets_path(), json)?;
     Ok(())
 }
 
 pub fn load_presets() -> Vec<Preset> {
-    let path = Path::new(FILE_NAME);
+    let path = presets_path();
 
     if !path.exists() {
         return Vec::new();
